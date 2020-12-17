78be0fa9ffd32aa12f5c827c2b28f1b06f1b8b2c
Jesús Enrique Rascón <blograso@gmail.com>
2020-12-18 00:18:59 +0100

    made some changes to experience, lets try CICD

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,9 +23,9 @@ module.exports = {
     //   xl: "1280px",
     // },
     extend: {
-      fontFamily: {
-        "firago": ["FiraGO", ...defaultTheme.fontFamily.sans],
-      },
+      // fontFamily: {
+      //   "firago": ["FiraGO", ...defaultTheme.fontFamily.sans],
+      // },
       fontSize: {
         sm2: "0.9375rem", // 15px label
         base: "1rem", // 16px base
